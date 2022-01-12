87bbbe43e81466f9976ee431f2e474e3dd9034a9
kadilan <yes235817@gmail.com>
2022-01-12 15:32:04 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -2,8 +2,18 @@ import streamlit as st
 import pandas as pd
 
 st.title('20171234 박상현')
-st.write("Here's our first attempt at using data to create a table:")
+
+st.header("Test Header")
+st.subheader("Test Subheader")
+
+st.write("Pandas table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
